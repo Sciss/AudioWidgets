fd9af2cbf29adcbc8f4874aa0b534e47d5dc299a
Sciss <contact@sciss.de>
2011-06-24 20:30:27 +0100

    package

--- src/main/scala/de/sciss/gui/j/AudioWidgets.scala
+++ src/main/scala/de/sciss/gui/j/AudioWidgets.scala
@@ -1,14 +1,49 @@
-package de.sciss.gui
+/*
+ *  AudioWidgets.scala
+ *  (AudioWidgets)
+ *
+ *  Copyright (c) 2011 Hanns Holger Rutz. All rights reserved.
+ *
+ *	This software is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License
+ *	as published by the Free Software Foundation; either
+ *	version 2, june 1991 of the License, or (at your option) any later version.
+ *
+ *	This software is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *	General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public
+ *	License (gpl.txt) along with this software; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *	For further information, please contact Hanns Holger Rutz at
+ *	contact@sciss.de
+ */
+
+package de.sciss.gui.j
 
 import java.awt.{EventQueue, BorderLayout}
 import javax.swing.{Timer, JFrame, WindowConstants}
 import java.awt.event.{WindowEvent, WindowAdapter, ActionEvent, ActionListener}
 
 object AudioWidgets extends App with Runnable {
+   val name          = "AudioWidgets"
+   val version       = 0.10
+   val copyright     = "(C)opyright 2011 Hanns Holger Rutz"
+   var isSnapshot    = false
+
    EventQueue.invokeLater( this )
 
+   def versionString = {
+      val s = (version + 0.001).toString.substring( 0, 4 )
+      if( isSnapshot ) s + "-SNAPSHOT" else s
+   }
+
    def run() {
-      val f    = new JFrame( "AudioWidgets" )
+      val f    = new JFrame( name )
       val cp   = f.getContentPane
       val m    = new PeakMeter
       m.ticks  = 60
